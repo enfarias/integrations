2038c7eca4d57ca37fd710b205ac49f352f30da9
Edson Ney de Farias <edson.ney@gmail.com>
2026-08-11 19:38:01 -0300

    feat: exception handling

--- src/main/java/com/treinamento/integrations/services/EmailService.java
+++ src/main/java/com/treinamento/integrations/services/EmailService.java
@@ -15,6 +15,7 @@ import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 import com.treinamento.integrations.dto.EmailDTO;
+import com.treinamento.integrations.services.exceptions.EmailException;
 
 @Service
 public class EmailService {
@@ -39,13 +40,13 @@ public class EmailService {
 			Response response = sendGrid.api(request);
 			if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
 				LOG.error("Error sending email: " + response.getBody());
+				throw new EmailException(response.getBody());
 			}
-			else {
-				LOG.info("Email sent! Status = " + response.getStatusCode());
-			}
-			
-		} catch (IOException e) {
+			LOG.info("Email sent! Status = " + response.getStatusCode());	
+		} 
+		catch (IOException e) {
 			e.printStackTrace();
+			throw new EmailException(e.getMessage());
 		}
 	}
 
